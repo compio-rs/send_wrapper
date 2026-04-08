77facb1252ea7e73cdf1bd1246dbfdf001b466fc
George Miao <gm@miao.dev>
2026-04-07 22:18:14 -0400

    feat: merge loom & other

--- src/lib.rs
+++ src/lib.rs
@@ -82,7 +82,10 @@
 //! [`Stream`]: futures_core::Stream
 // To build docs locally use `RUSTDOCFLAGS="--cfg docsrs" cargo doc --open --all-features`
 #![cfg_attr(docsrs, feature(doc_cfg))]
-#![cfg_attr(feature = "current_thread_id", feature(current_thread_id))]
+#![cfg_attr(
+    all(not(loom), feature = "current_thread_id"),
+    feature(current_thread_id)
+)]
 #![warn(missing_docs)]
 
 #[cfg(feature = "futures")]
@@ -96,38 +99,22 @@ use std::{
 };
 
 cfg_if::cfg_if! {
-    if #[cfg(loom)] {
-        use loom::{
-            cell::Cell,
-            thread::{self, ThreadId},
-        };
-
-        loom::thread_local! {
-            static THREAD_ID: Cell<ThreadId> = Cell::new(thread::current().id());
-        }
-
-        /// Get the current mocked [`ThreadId`].
-        pub fn current_id() -> ThreadId {
-            THREAD_ID.with(|id| id.get())
-        }
-
-    } else if #[cfg(feature = "current_thread_id")] {
-        use std::thread::{self, current_id, ThreadId};
-
-    } else {
-        use std::{
-            cell::Cell,
-            thread::{self, ThreadId},
-        };
+    if #[cfg(any(loom, not(feature = "current_thread_id")))] {
+        #[cfg(loom)]
+        use loom::{thread_local, cell::Cell, thread::{self, ThreadId}};
+        #[cfg(not(loom))]
+        use std::{thread_local, cell::Cell, thread::{self, ThreadId}};
 
         thread_local! {
             static THREAD_ID: Cell<ThreadId> = Cell::new(thread::current().id());
         }
 
         /// Get the current [`ThreadId`].
-        pub fn current_id() -> ThreadId {
-            THREAD_ID.get()
+        pub(crate) fn current_id() -> ThreadId {
+            THREAD_ID.with(|id| id.get())
         }
+    } else {
+        use std::thread::{self, current_id, ThreadId};
     }
 }
 
